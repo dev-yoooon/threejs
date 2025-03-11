4762ad9c0c6a337e5fbe03b365262dd4bae8f445
yoooon <dev.yoooon@gmail.com>
2025-03-11 23:14:21 +0900

    test

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   // css: { preprocessorOptions: { scss: { charset: false } } },
+  optimizeDeps: {
+    include: ["three"],
+  },
 })
