ea8cd6c4a98f17b0038bbd1a50965710448b5dc9
yoooon <dev.yoooon@gmail.com>
2025-03-11 23:27:09 +0900

    test

--- index2.js
+++ index2.js
@@ -2,13 +2,13 @@ import { Scene, WebGLRenderer, PerspectiveCamera, Color, DirectionalLight, sRGBE
 import * as three from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import * as lil from 'lil-gui';
+// import * as lil from 'lil-gui';
 import './style.scss';
 
 let clock, mixer, controls;
 
 // debug
-const gui = new lil.GUI;
+// const gui = new lil.GUI;
 
 //  canvas
 const canvas = document.querySelector('#canvas');
@@ -34,8 +34,8 @@ const light = new three.AmbientLight( 0xffffff );
 const pointLight = new three.PointLight(0xffffff, 1)
 // pointLight.position.set(0, 2, 12)
 scene.add(pointLight)
-
 scene.add(light);
+
 
 // texture
 const textureLoader = new three.TextureLoader();
@@ -44,8 +44,6 @@ const textureMew = textureLoader.load('./model/mew/textures/material_0_baseColor
 // controls
 renderer.outputEncoding = three.sRGBEncoding;
 controls = new OrbitControls(camera, renderer.domElement)
-
-console.log(controls);
 
 clock = new three.Clock();
 
@@ -67,25 +65,28 @@ function model(){
         
         // model.position.y = -10;
         model.rotation.y = 0;
-        model.scale.set(1.1, 1.1, 1.1);
+        model.scale.set(1, 1, 1);
+        // model.position.set(0,-0.04,0);
+
         // textureMew
         //텍스쳐 적용
         model.traverse((child) => {
             if (child.isMesh) {
             // child.material = child.material.clone();
-            child.material.map = textureMew;
+            // child.material.map = textureMew;
             }
         });
 
         // debug
-        gui.add(model.position, 'y', -1000, 1000, 1).name('model position y');
-        gui.add(model.position, 'z', 0, 1000, 1).name('model position z');
-        gui.add(model.rotation, 'y', 0, 1000, 1).name('model rotation y');
-        gui.add(camera.position, 'y', 0, 1000, 1).name('camera position y');
-        gui.add(camera.position, 'z', 0, 1000, 1).name('camera position z');
+        // gui.add(model.position, 'y', -1000, 1000, 1).name('model position y');
+        // gui.add(model.position, 'z', 0, 1000, 1).name('model position z');
+        // gui.add(model.rotation, 'y', 0, 1000, 1).name('model rotation y');
+        // gui.add(camera.position, 'y', 0, 1000, 1).name('camera position y');
+        // gui.add(camera.position, 'z', 0, 1000, 1).name('camera position z');
 
         scene.add( model );
         ZoomFit(model, camera);
+        animate();
         
         function animate(){
             requestAnimationFrame(animate);
@@ -96,8 +97,7 @@ function model(){
             }
             renderer.render( scene, camera );
         }
-        animate();
-        console.log(model.rotation);
+        
     });
 }
 
